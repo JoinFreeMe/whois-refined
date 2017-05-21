9c0303e0ed2841852b8f1e68974018e6020b1428
FreeMe <info@lewis-hallam.co.uk>
2017-05-21 19:32:09 +0100

    initial upload

--- installer.sh
+++ installer.sh
@@ -7,7 +7,7 @@ read -p "what is your username: " user
 #alias does not allow params by default. let's create a function for it
 echo "whois-refined() {
 whois "cannotpassdollaronelel" | grep -E 'city|address'
-}" > ~/.bashrc
+}" >> ~/.bashrc
 sed -i 's/cannotpassdollaronelel/$1/' /home/$user/.bashrc
 
 source /home/$user/.bashrc
